440fdc5f99c789f000d0a344113315e8c24259a2
kdybvig <kdybvig@Keiths-MacBook-Pro.local>
2019-05-19 13:22:07 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav'
 
 function App() {
-
+  const apiKey = "71ea7290"
+  useEffect(() => {
+    fetch('http://www.omdbapi.com/?s=waterboy&apikey=' + apiKey)
+    .then(res => res.json())
+    .then(response => {
+      console.log(response)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  }, [])
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <Nav></Nav>
+      <div>Hello World</div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav = props => {
+
+    return (
+      <nav>
+        <h5>Movie Queue and Review Application</h5>
+
+
+      </nav>  
+    )
+}
+
+export default Nav;
